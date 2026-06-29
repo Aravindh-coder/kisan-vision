61792f64f6bc6887fdb4f6ac65959d676dba3c54
Your Name <your-github-email@example.com>
2026-06-29 23:35:11 +0530

    fix: mysql ssl connection for aiven

--- server/db/index.ts
+++ server/db/index.ts
@@ -4,5 +4,9 @@ import * as schema from './schema'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const pool = mysql.createPool(process.env.DATABASE_URL!)
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL!.replace('?ssl-mode=REQUIRED', ''),
+  ssl: { rejectUnauthorized: false }
+})
+
 export const db = drizzle(pool, { schema, mode: 'default' })
